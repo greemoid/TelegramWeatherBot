e5a560bb76d6e507113dd1f8e43d6ab15667decb
greemoid <63496492+greemoid@users.noreply.github.com>
2022-07-08 18:40:49 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,7 @@ import data.api.RetrofitInstance
 import data.repository.WeatherRepository
 
 fun main(args: Array<String>) {
-    println("Hello World!")
-
+    
     val weatherApi = RetrofitInstance.api
     val weatherRepository = WeatherRepository(weatherApi)
     val weatherBot = WeatherBot(weatherRepository = weatherRepository).createBot()
